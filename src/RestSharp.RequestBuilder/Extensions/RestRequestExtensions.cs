acc9f1865d23be5cadacd42bb7411e66859f87a3
Brandon Henricks <brandon.henricks1984@gmail.com>
2025-01-30 10:56:29 -0500

    Add overloads to WithBuilder and new RequestBuilder ctor
    Introduce new overloads for the WithBuilder extension method in
    RestRequestExtensions to support resource as string or Uri with
    an HTTP method. Add a new constructor to RequestBuilder to
    initialize with a Uri and HTTP method, ensuring Uri is not null
    and converting it to a string for internal use.

--- src/RestSharp.RequestBuilder/Extensions/RestRequestExtensions.cs
+++ src/RestSharp.RequestBuilder/Extensions/RestRequestExtensions.cs
@@ -29,5 +29,29 @@ namespace RestSharp.RequestBuilder.Extensions
         {
             return new RequestBuilder(resource);
         }
+
+        /// <summary>
+        /// Creates a new <see cref="IRequestBuilder"/> for the specified resource and HTTP method.
+        /// </summary>
+        /// <param name="request">The <see cref="RestRequest"/> instance.</param>
+        /// <param name="resource">The resource as a string.</param>
+        /// <param name="method">The HTTP method to use for the request.</param>
+        /// <returns>A new instance of <see cref="IRequestBuilder"/>.</returns>
+        public static IRequestBuilder WithBuilder(this RestRequest request, string resource, Method method)
+        {
+            return new RequestBuilder(resource, method);
+        }
+
+        /// <summary>
+        /// Creates a new <see cref="IRequestBuilder"/> for the specified resource and HTTP method.
+        /// </summary>
+        /// <param name="request">The <see cref="RestRequest"/> instance.</param>
+        /// <param name="resource">The resource as a <see cref="Uri"/>.</param>
+        /// <param name="method">The HTTP method to use for the request.</param>
+        /// <returns>A new instance of <see cref="IRequestBuilder"/>.</returns>
+        public static IRequestBuilder WithBuilder(this RestRequest request, Uri resource, Method method)
+        {
+            return new RequestBuilder(resource, method);
+        }
     }
 }
